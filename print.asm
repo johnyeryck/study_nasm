be068a43eb8f5cfa4c22e4f3265d48c33d1ae03f
Johnyeryck <johnybombado@gmail.com>
2026-09-15 14:38:17 -0300

    Change usage message and adjust argument processing
    Updated usage message and modified argument handling in print.asm.

--- print.asm
+++ print.asm
@@ -1,5 +1,5 @@
 section .data
-msg db "Usage : ./print <string> <type>",0
+msg db "Usage : ./print <string> <string length>",0
 len equ $ - msg
 section .text
 
@@ -9,13 +9,25 @@ _start:
 mov r8,[rsp]
 cmp r8,3
 jne exit
-mov r8,[rsp + 10]
-add r8,8
+mov r8,[rsp + 18] // argv[1]
+mov r9,r8 + 8
+
+cmp [r9],0
+jg 
+
 
 mov rax,60
 xor rdi,rdi
 syscall
 
+print_char:
+mov rax,1
+mov rdi,1
+mov rsi,[r8]
+mov rdx,1
+inc r9
+syscall
+ret
 
 exit:
 mov rax,1
