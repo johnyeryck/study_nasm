0d4772aadc6e407e9b4a2a9c75ea9e86f895076b
Johnyeryck <johnybombado@gmail.com>
2026-09-16 03:28:04 -0300

    Refactor print.asm to simplify usage and logic
    Removed decimal printing functionality and updated usage message.

--- print.asm
+++ print.asm
@@ -1,5 +1,5 @@
 section .data
-msg db "Usage : ./print <string> <type>",0
+msg db "Usage : ./print <string> ",0
 len equ $ - msg
 
 section .text
@@ -8,25 +8,16 @@ global _start
 
 _start:
 mov r8,[rsp]
-cmp r8,3
+cmp r8,2
 jne usage
 
-mov qword r8,[rsp + 16]
-mov qword r9,[rsp + 24]
-
-sub byte [r9],48
-mov r10,[r9]
-call loop
+mov r8,[rsp + 16]
+jmp loop
 
 loop:
 cmp byte [r8],0
 je exit
-cmp  r10,1
-je print_char
-pop r10
-cmp  r10,2
-je print_dec
-call exit
+jmp print_char
 
 print_char:
 mov rax,1
@@ -37,15 +28,6 @@ syscall
 inc r8
 jmp loop
 
-print_dec:
-sub [r8],48
-mov rax,1
-mov rdi,1
-mov rsi,r8
-mov rdx,1
-syscall
-inc r8
-jmp loop
 
 usage:
 mov rax,1
